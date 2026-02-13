55fd95e32475864c6ce71e5cc30bf560bbb1421c
BurN-30 <saccol.nathan@yahoo.fr>
2026-02-13 17:07:22 +0100

    ✨ ajustement accueil: 3 derniers posts

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Post;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,10 +11,16 @@ use Symfony\Component\Routing\Attribute\Route;
 final class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(): Response
+    public function index(EntityManagerInterface $em): Response
     {
+        $posts = $em->getRepository(Post::class)->findBy(
+            ['isPublished' => true],
+            ['createdAt' => 'DESC'],
+            3
+        );
+
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            'posts' => $posts,
         ]);
     }
 }
